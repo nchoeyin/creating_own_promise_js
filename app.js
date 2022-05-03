3b2529aa148acc574e40b3b7675771eb1a0a56ee
Ngawang C <ngawangchoeying303@gmail.com>
2022-05-03 23:59:23 +0530

    Only created own promise

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+// const delayedColourChange = (color,delay) => {
+//     return new Promise((resolve,reject) => {
+//         setTimeout(()=> {
+//             document.body.style.backgroundColor= color;
+//             resolve();
+//         },delay)
+//     })
+// }
+
+// delayedColourChange('red',1000)
+//     .then (()=> delayedColourChange('vioet',1000))
+//     .then (()=> delayedColourChange('yellow',1000))
+//     .then (()=> delayedColourChange('green',1000))
+//     .then (()=> delayedColourChange('indigo',1000))
+//     .then (()=> delayedColourChange('violet',1000))
+//     .then (()=> delayedColourChange('orange',1000))
+//     .then (()=> delayedColourChange('yellow',1000))
